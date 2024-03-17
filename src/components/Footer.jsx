17f990f1950a99a33d903b8470e8ca7474c40562
tanaySharma <Tanay Sharma@LAPTOP-JTQ66A27>
2024-03-17 13:46:59 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { FiHeart } from 'react-icons/fi'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+        <h4>Made by TANAY SHARMA</h4>
+        <FiHeart className='heart'></FiHeart>
+    </div>
+  )
+}
+
+export default Footer
